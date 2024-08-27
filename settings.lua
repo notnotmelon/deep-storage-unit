0c8fa1aac5519c5dabcd973787393ee1e31e33d1
Zachary Picco <zacharypicco@gmail.com>
2024-08-27 15:24:34 -0500

    feat: Added additional power consumption options in the mod settings.…  … Now you can go all the way up to 50MW. (Previous limit was 2.4MW)

--- settings.lua
+++ settings.lua
@@ -3,5 +3,5 @@ data:extend{{
 	name = 'memory-unit-power-usage',
 	setting_type = 'runtime-global',
 	default_value = '300kW',
-	allowed_values = {'0W', '60kW', '180kW', '300kW', '480kW', '600kW', '1.2MW', '2.4MW'}
+	allowed_values = {'0W', '60kW', '180kW', '300kW', '480kW', '600kW', '1.2MW', '2.4MW', '3.6MW', '5MW', '10MW', '20MW', '50MW'}
 }}
